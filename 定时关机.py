a6c7ae4d02900fa67f4f0f3c191672472cac1747
代强 <daiqianghaha@foxmail.com>
2018-06-23 22:04:07 +0800

    fix bug
    fix bug

--- 定时关机.py
+++ 定时关机.py
@@ -7,7 +7,7 @@ def time_to_sec(t):
 def main():
 	os.system("title 代强的定时关机程序")
 	os.system("color 9e")
-	shut_time=input("输入关机时间或者是距离关机多少小时:\n")
+	shut_time=input("输入关机时间或者是距离关机多少秒:\n")
 	if ':'in shut_time:
 		second=time_to_sec(shut_time)-time_to_sec(time.strftime("%H:%M", time.localtime()))
 		powerdown_time=shut_time
