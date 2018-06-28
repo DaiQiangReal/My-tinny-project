a03139da90c511037bda657cbf921b8fcb1a2777
代强 <daiqianghaha@foxmail.com>
2018-06-28 15:57:51 +0800

    添加了自动插入程序运行截图的功能
    添加了自动插入程序运行截图的功能

--- 代强的实验报告自动生成器.py
+++ 代强的实验报告自动生成器.py
@@ -6,14 +6,20 @@ def sor(name):
 	else:
 		return name[0:2]
 
+def picname(name):
+	if name[3]=='.':
+		return name[0:3]
+	else:
+		return name[0:4]
+
 def main():
-	print("			代强的实验报告自动生成器\n")
+	print("			代强的实验报告自动生成器\n代码格式为1.1.c 截图格式为1.1.PNG\n")
 	os.system("title 代强的实验报告自动生成器")
 	os.system("color 2e")
 	belong='1'
 	pages=docx.Document()
 	path=input("输入你的代码的路径\n")
-	print("\n")
+	picpath=input("输入你截图的路径\n")
 	anspath=input("输入需要存放实验报告的地方\n")
 	change={'1':'实验一','2':'实验二','3':'实验三','4':'实验四','5':'实验五','6':'实验六',
 	'7':'实验七','8':'实验八','9':'实验九','10':'实验十','11':'实验十一','12':'实验十二',
@@ -24,6 +30,7 @@ def main():
 			text=f.read()
 			pages.add_paragraph(str(some))
 			pages.add_paragraph(text)
+			pages.add_picture(picpath+'\\'+picname(some)+'.PNG')
 			pages.add_paragraph("******************************************************************\n")
 			if files.index(some)+1==len(files):
 				pages.save(anspath+"\\"+change[sor(files[files.index(some)])]+".docx")
